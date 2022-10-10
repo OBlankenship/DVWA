59f60e243485716830b051ea6d8620ad4f368598
michaelujkim <michaelujkim@gmail.com>
2022-10-10 13:20:21 -0700

    file inclusion patch added

--- vulnerabilities/fi/source/secure.php
+++ vulnerabilities/fi/source/secure.php
@@ -3,4 +3,10 @@
 // The page we wish to display
 $file = $_GET[ 'page' ];
 
+// Input validation
+if( !fnmatch( "file*", $file ) && $file != "include.php" ) {
+	
+	echo "ERROR: File not found!";
+	exit;
+}
 ?>
