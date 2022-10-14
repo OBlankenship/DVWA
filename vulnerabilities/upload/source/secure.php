56d6cca4293cd6118965dd431df46034b8f380de
OBlankenship <oliverblankenship@gmail.com>
2022-10-14 00:42:55 -0400

    Added mime-content verification for file upload. Modified upload folder for file upload.

--- vulnerabilities/upload/source/secure.php
+++ vulnerabilities/upload/source/secure.php
@@ -2,17 +2,28 @@
 
 if( isset( $_POST[ 'Upload' ] ) ) {
 	// Where are we going to be writing to?
-	$target_path  = DVWA_WEB_PAGE_TO_ROOT . "hackable/uploads/";
+	$target_path  = "/srv/images/";
 	$target_path .= basename( $_FILES[ 'uploaded' ][ 'name' ] );
 
-	// Can we move the file to the upload folder?
-	if( !move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path ) ) {
-		// No
-		$html .= '<pre>Your image was not uploaded.</pre>';
+	$mime_content_type = mime_content_type($_FILES[ 'uploaded' ][ 'tmp_name' ]);
+
+	// Invalid mime_content_type
+	if ($mime_content_type != "image/jpeg" and $mime_content_type != "image/png") 
+	{
+		$html .= '<pre>Invalid file type. We only support .jpg and .png files!</pre>';
 	}
-	else {
-		// Yes!
-		$html .= "<pre>{$target_path} succesfully uploaded!</pre>";
+	// Valid mime_content_type
+	else 
+	{
+		// Can we move the file to the upload folder?
+		if( !move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path ) ) {
+			// No
+			$html .= '<pre>Your image was not uploaded.</pre>';
+		}
+		else {
+			// Yes!
+			$html .= "<pre>File succesfully uploaded!</pre>";
+		}
 	}
 }
 
