3796a3a780adc3e7bdbb4a7e4b345997296ac4da
michaelujkim <michaelujkim@gmail.com>
2022-10-17 11:16:49 -0700

    comments

--- vulnerabilities/fi/source/secure.php
+++ vulnerabilities/fi/source/secure.php
@@ -1,9 +1,9 @@
 <?php
 
-// The page we wish to display
+// getting input
 $file = $_GET[ 'page' ];
 
-// Input validation
+// line below takes the input and checks if it has file in it and include.php
 if( !fnmatch( "file*", $file ) && $file != "include.php" ) {
 	
 	echo "ERROR: Trying to acces Unauthorized Data";
