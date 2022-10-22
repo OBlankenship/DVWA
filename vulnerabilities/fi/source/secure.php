09f52c96c7657575a1f56b0b4021cd3caee9ee91
michaelujkim <michaelujkim@gmail.com>
2022-10-22 06:24:10 -0700

    command inj

--- vulnerabilities/fi/source/secure.php
+++ vulnerabilities/fi/source/secure.php
@@ -6,7 +6,7 @@ $file = $_GET[ 'page' ];
 // line below takes the input and checks if it has file in it and include.php
 if( !fnmatch( "file*", $file ) && $file != "include.php" ) {
 	
-	echo "ERROR: Trying to acces Unauthorized Data";
+	echo "ERROR: Trying to access Unauthorized Data";
 	exit;
 }
 ?>
