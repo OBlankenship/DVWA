abef6e325fe6ef4562bdb834eb29822193a71497
marlee114 <71605411+marlee114@users.noreply.github.com>
2022-11-06 21:44:26 -0500

    Update secure.php

--- vulnerabilities/xss_r/source/secure.php
+++ vulnerabilities/xss_r/source/secure.php
@@ -4,6 +4,12 @@ header ("X-XSS-Protection: 0");
 
 // Is there any input?
 if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
+	
+	// Get input
+	$name = str_replace( '<img>', '', $_GET[ 'name' ] );
+	$name = str_replace( '<script>', '', $_GET[ 'name' ] );
+	
+	
 	// Feedback for end user
 	$html .= '<pre>Hello ' . $_GET[ 'name' ] . '</pre>';
 }
